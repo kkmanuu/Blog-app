82b7bd639b8216992b89c3d9594f87232fa7326e
Bettenoch <kipngenoemmanuel479@gmail.com>
2023-10-03 20:37:17 +0300

    fix the linters errors

--- db/migrate/20231003124703_cast_posts_counter_type.rb
+++
@@ -1,9 +0,0 @@
-class CastPostsCounterType < ActiveRecord::Migration[7.0]
-  def up
-    change_column :users, :posts_counter, :integer, using: 'posts_counter::integer'
-  end
-
-  def down
-    change_column :users, :posts_counter, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_231_003_124_753) do
+
+ActiveRecord::Schema[7.0].define(version: 20_230_925_223_129) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension 'plpgsql'
   create_table 'comments', force: :cascade do |t|
     t.integer 'author_id', null: false
     t.integer 'post_id', null: false
