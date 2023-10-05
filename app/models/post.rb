0b51292db3fdaba11aa8d8276e4c8a4011641ac2
Bettenoch <kipngenoemmanuel479@gmail.com>
2023-10-05 18:42:23 +0300

    Fix the linters

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,21 +1,21 @@
 class Post < ApplicationRecord
-    belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-    has_many :comments
-    has_many :likes
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  has_many :comments
+  has_many :likes
 
-    validates :title, presence: true, length: { maximum: 250 }
-    validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-    validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-    after_save :update_author_counter
+  after_save :update_author_counter
 
-    def recent_comments(limit = 5)
-      comments.order(created_at: :desc).limit(limit)
-    end
+  def recent_comments(limit = 5)
+    comments.order(created_at: :desc).limit(limit)
+  end
 
-    private
+  private
 
-    def update_author_counter
-      author.increment!(:posts_counter)
+  def update_author_counter
+    author.increment!(:posts_counter)
   end
 end
