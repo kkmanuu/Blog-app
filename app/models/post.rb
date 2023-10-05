328941eab9ff07710b14464b1c48ab905efd4dcc
Bettenoch <kipngenoemmanuel479@gmail.com>
2023-10-05 18:10:07 +0300

    Refactor Post model for author counter update after save

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,22 +1,21 @@
 class Post < ApplicationRecord
-  has_many :comments
-  has_many :likes
-  belongs_to :author, class_name: 'User'
+    belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+    has_many :comments
+    has_many :likes
 
-  attribute :title, :string
-  attribute :text, :text
-  attribute :comments_counter, :integer, default: 0
-  attribute :likes_counter, :integer, default: 0
+    validates :title, presence: true, length: { maximum: 250 }
+    validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+    validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  after_save :update_post_counter
+    after_save :update_author_counter
 
-  private
+    def recent_comments(limit = 5)
+      comments.order(created_at: :desc).limit(limit)
+    end
 
-  def update_post_counter
-    author.increment!(:posts_counter)
-  end
+    private
 
-  def five_most_recent_comments
-    comments.order(created_at: :desc).limit(5)
+    def update_author_counter
+      author.increment!(:posts_counter)
   end
 end
