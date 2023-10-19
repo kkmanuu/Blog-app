633f4ebba357b95d37a267b6551f787b31ee8d7a
Bettenoch <kipngenoemmanuel479@gmail.com>
2023-10-19 13:36:07 +0300

    solve the linters , add styling and modify the files

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,11 +2,3 @@ class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
   layout 'mailer'
 end
-email.changed.html.rb
-     <% if @resource.try(:unconfirmed_email?) %>
-  <p>We're contacting you to notify you that your email is being changed to <%= @resource.unconfirmed_email %>.</p>
-<% else %>
-  <p>We're contacting you to notify you that your email has been changed to <%= @resource.email %>.</p>
-<% end %>
-another file called (password_changed.html.rb)
-  <p>We're contacting you to notify you that your password has been changed.</p>
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,3 +14,4 @@ class User < ApplicationRecord
     posts.order(created_at: :desc).limit(3)
   end
 end
+
